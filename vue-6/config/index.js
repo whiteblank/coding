579d66a2a96f9c488a361587bd33d91f4fe825b4
whiteblank <824936522@qq.com>
2017-02-06 19:09:15 +0800

    Add files via upload

--- vue-6/config/index.js
+++ vue-6/config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resume/dist',
+    assetsPublicPath: 'vue-6/resume/dist',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
